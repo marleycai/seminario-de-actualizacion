0db05e74b34dccf20a75912e074c0b0934700e34
marleycai <facu.gcaminos@gmail.com>
2023-06-04 11:20:27 -0300

    avances ,nuevas tablas y consultas ,tratamos de ir relacionando los recursos , con persmisos y usuarios

--- access-control-module/scrypt.sql
+++ access-control-module/scrypt.sql
@@ -21,3 +21,38 @@ CREATE TABLE usuarios_grupos (
   FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
   FOREIGN KEY (grupo_id) REFERENCES grupos(id)
 );
+
+CREATE TABLE sesiones (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario_id INT,
+  fecha_inicio DATETIME,
+  fecha_fin DATETIME,
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+
+CREATE TABLE auditoria (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario_id INT,
+  fecha_hora DATETIME,
+  accion ENUM('login', 'logout'),
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE recursos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(100)
+  -- Otros campos relevantes para tus recursos
+);
+
+CREATE TABLE permisos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario_id INT,
+  recurso_id INT,
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+  FOREIGN KEY (recurso_id) REFERENCES recursos(id)
+);
+INSERT INTO permisos (usuario_id, recurso_id) VALUES (1, 2);
+SELECT COUNT(*) FROM permisos WHERE usuario_id = 1 AND recurso_id = 2;
+DELETE FROM permisos WHERE usuario_id = 1 AND recurso_id = 2;
